ad95ad0db18f612a560ee99cce367dc1681761b9
shobi98 <chausshoaib90@gmail.com>
2020-06-26 18:26:09 +0530

    Create a Stateless Functional Component

---
+++ Create a Stateless Functional Component.js
@@ -0,0 +1,13 @@
+const MyComponent = function() {
+    // change code below this line
+    
+    return(
+      <div className='customClass'>
+      demo
+      </div>
+    );
+  
+  
+    // change code above this line
+  }
+  
